7cabde7fdfa1d22129b301c44d04ccc1ea91a62e
Harshit Sharma <18deepanshu11@gmail.com>
2024-04-11 21:41:26 +0530

    Webinar Section Added

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -16,7 +16,7 @@ export default function Navbar({ className }: { className?: string }) {
   return (
     <div
       className={cn(
-        "fixed top-10 inset-x-0 max-w-2xl mx-auto z-50  ",
+        "fixed top-10 inset-x-0 max-w-2xl mx-auto z-50 ",
         className
       )}
     >
